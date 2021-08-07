5d6f5580c04f4343e57d15e768d72c2bf336c10f
NotCoded <66999075+not-coded@users.noreply.github.com>
2021-08-07 21:04:29 +0000

    Added info to make it easier to understand.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const keepAlive = require("./server.js")
-const TOKEN = process.env['TOKEN']
-const PREFIX = process.env['PREFIX']
-
+const TOKEN = process.env['TOKEN'] // Used to login the bot
+const PREFIX = process.env['PREFIX'] // Used for commands
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -18,26 +16,26 @@ client.on('ready', () => {
     });
 });
 
-client.login(TOKEN);
+client.login(TOKEN); // Logs the bot in
 
-keepAlive()
+keepAlive() // Keeps the bot alive
 
 client.on('message', msg => {
     const HelpEmbed = new Discord.MessageEmbed()
-    .setColor('#ffff4d')
+    .setColor('#ffff4d') // Light yellow
     .setTitle('Commands:')
     .addField('Misc:', 'd!ip, d!ad, d!invite, d!apply')
 
-    .setFooter('I hope this helped!', (msg.author.avatarURL()))
+    .setFooter('I hope this helped!', (msg.author.avatarURL())) // User's avatar
 
     const IPEmbed = new Discord.MessageEmbed()
-    .setColor('#ffff4d')
+    .setColor('#ffff4d') // Light yellow
     .setTitle('The Server IP:')
     .addField('DuckDupes.minehut.gg', 'Version: 1.17+')
     .addField('Syphere.minehut.gg', 'Version: 1.17+')
 
     const AdEmbed = new Discord.MessageEmbed()
-    .setColor('#ffff4d')
+    .setColor('#ffff4d') // Light yellow
     .setTitle('Advertise the server:')
     .addField("DuckDupes:", 'Advertising on the server: DuckDupes!')
     .addField('Default:', '/ad DuckDupes for a server with Random Items /Dupe Void and more!')
@@ -46,19 +44,18 @@ client.on('message', msg => {
     .addField('Default:', '/ad Syphere Can you survive in Bedrock &cSphere without dying?')
     .addField ('VIP:', '/ad Syphere &cCan you survive in a &8Bedrock &cSphere without &c&ldying?')
 
-    .setThumbnail('https://cdn.discordapp.com/icons/737765981807706142/66a075ffe40c1c1a5925c26a432fa6ab.png?size=128')
+    .setThumbnail('https://cdn.discordapp.com/icons/737765981807706142/66a075ffe40c1c1a5925c26a432fa6ab.png?size=128') // Server icon
 
 
     if (msg.content === `${PREFIX}help`) {
-        msg.channel.send(HelpEmbed);
+        msg.channel.send(HelpEmbed); // Sends the help embed
     } else if (msg.content === `${PREFIX}ip`) {
-        msg.channel.send(IPEmbed);
+        msg.channel.send(IPEmbed); // Sends the ip embed
     } else if (msg.content === `${PREFIX}ad`) {
-        msg.channel.send(AdEmbed);
+        msg.channel.send(AdEmbed); // Sends the Advertisement embed
     } else if (msg.content === `${PREFIX}invite`) {
-        msg.channel.send("https://discord.gg/TeVApzcB3n");
+        msg.channel.send("https://discord.gg/TeVApzcB3n"); // Sends the invite link
     } else if (msg.content === `${PREFIX}apply`) {
-        msg.channel.send("https://forms.gle/nxCQqNWtdcP3qfmB6");
-        
+        msg.channel.send("https://forms.gle/nxCQqNWtdcP3qfmB6"); // Sends the forum link
     }
 })
