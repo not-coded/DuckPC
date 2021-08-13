cecc8230cd36aa580e6c79e2460f50b47f8f0b99
NotCoded <66999075+not-coded@users.noreply.github.com>
2021-08-13 18:50:45 +0000

    Made it show the prefix on the console log when starting the bot.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}, and the prefix is "${PREFIX}command"`);
 
     client.user.setPresence({
         status: 'online',
