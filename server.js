f8b0b7670ca535b264eb55d15785a54bec16eb76
NotCoded <66999075+not-coded@users.noreply.github.com>
2021-08-14 20:04:24 +0000

    Attempted to add an favicon to the website/html but failed.

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const favicon = require('serve-favicon');
 const path = require('path')
 const app = express()
 
-server.all('/', (req, res)=>{
-    // res.send('Your bot is alive!')
-    res.sendFile(__dirname + "/index.html");
+server.all('/', (req, res) => {
+  // app.use(favicon(path.join(__dirname, 'public', 'duckdupes.ico')))
+  // app.use("/public", express.static('public'));
+  // app.use(favicon(path.join(__dirname, 'public', 'duckdupes.ico')))
+  res.sendFile(__dirname + "/index.html");
 })
-function keepAlive(){
-    server.listen(3000, ()=>{console.log("Server is Ready!")});
+function keepAlive() {
+  server.listen(3000, () => { console.log("Server is Ready!") });
 }
 module.exports = keepAlive;
